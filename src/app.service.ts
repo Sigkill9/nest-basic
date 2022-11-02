029b0d5c1a7fda3f3f9953d7e185dc93a2ecee35
JMART <register@truncator.com>
2022-11-02 17:18:15 -0500

    Initial

---
+++ src/app.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class AppService {
+  getRoot(): string {
+    return 'Gort, Klaatu. Barada. Nikto!';
+  }
+  getHello(): string {
+    return 'Hello, World!';
+  }
+}
